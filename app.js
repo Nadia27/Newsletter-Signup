d02b81fed20446938fb7e4f9e0a9ef5d34e06a29
Nadia27 <jones.nadia.l@gmail.com>
2019-10-09 10:16:14 -0400

    Fix port for heroku deployment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express'); // Server
 const bodyParser = require('body-parser'); // middleware: parse incoming requests
 const request = require('request'); // allows http requests
 
-const config = require('./config.js');
+const config = require('./config.js'); 
 
 const myKey = config.MAILCHIMP_API_KEY;
 const myList = config.MAILCHIMP_AUDIENCE_ID;
